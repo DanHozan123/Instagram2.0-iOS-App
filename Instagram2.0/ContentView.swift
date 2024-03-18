956ac33ef3b7e6ca69b7179e1ee61c1384d62239
DanHozan123 <danhozan@gmail.com>
2024-03-18 11:54:00 +0200

    Initial Commit

---
+++ Instagram2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Instagram2.0
+//
+//  Created by Dan Hozan on 18.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instagram2.0/Instagram2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram2_0App.swift
+//  Instagram2.0
+//
+//  Created by Dan Hozan on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Instagram2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
